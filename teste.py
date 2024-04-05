2771326f2e2a4b0288b7ab2481971c6e004335d9
Goho <richardgrlara@gmail.com>
2024-04-05 10:14:53 -0300

    update file

--- teste.py
+++ teste.py
@@ -1,2 +1,14 @@
+import os
+
 goho = "A verdade está lá fora..."
-print(goho)
+
+def limpar_tela():
+    # Verifica se o sistema operacional é Windows ou Linux/Mac
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    else:  # Linux/Mac
+        os.system('clear')
+# Chamada da função para limpar a tela
+limpar_tela()
+
+print(f"{goho}\n\n")
